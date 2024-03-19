0c2112c0bff14983a9491111148a3670f91f09db
Przemysław Kędzierski <przemekkt@gmail.com>
2024-03-20 00:45:13 +0100

    Remove old script

--- scripts/publish.sh
+++
@@ -1,3 +0,0 @@
-docker build --platform=linux/amd64 -t docker.hexagonale.net/jwt-keys-rotator:latest -t docker.hexagonale.net/jwt-keys-rotator:$1 .
-docker push docker.hexagonale.net/jwt-keys-rotator:$1
-docker push docker.hexagonale.net/jwt-keys-rotator:latest
