0c8c4a0a3ef428f3ba3c2febec1907ad538bd28a
Przemysław Kędzierski <przemekkt@gmail.com>
2024-03-23 13:13:31 +0100

    Add KEY_ALGORITHM env

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ const configSchema = z.object({
 	namespace: z.string(),
 	secretName: z.string(),
 	maxKeys: z.coerce.number().positive().int(),
+	keyAlgorithm: z.enum(['rsa-2048', 'rsa-4096', 'ed25519', 'ed448']),
 });
 
 type ConfigIntermediate = Record<keyof Config, string | undefined>;
@@ -15,6 +16,7 @@ export const configFactory = (): Config => {
 		namespace: process.env.NAMESPACE,
 		secretName: process.env.SECRET_NAME ?? 'jwt-keys',
 		maxKeys: process.env.MAX_KEYS ?? '2',
+		keyAlgorithm: process.env.KEY_ALGORITHM ?? 'ed25519',
 	};
 
 	return configSchema.parse(config);
